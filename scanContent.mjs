695ec036cad9ce884198549b4df171e6c62da34f
NikBit101 <n.cern.03@gmail.com>
2024-04-29 16:01:56 +0100

    fixed first-time scan bug

--- scanContent.mjs
+++ scanContent.mjs
@@ -10,10 +10,10 @@ function isMalicious(analysisResult) {
 	// Assign weights to each severity status
 	const maliciousWeight = 3;
 	const suspiciousWeight = 1;
-	
+
 	// Calculate the weighted sum
 	const weightedSum = (maliciousWeight * malCount) + (suspiciousWeight * susCount);
-
+	console.log(weightedSum);
 	if (weightedSum <= 1) { return 'clean'; }
 	else if (weightedSum <= 3) { return 'low'; }
 	else if (weightedSum <= 6) { return 'medium'; }
@@ -34,18 +34,21 @@ export async function scanURL(url) {
 	try {
 		const scanResult = await fetch('https://www.virustotal.com/api/v3/urls', options);
 		const response = await scanResult.json();
-		const analysisURL = response.data.links.self;
+		let analysisData;
 
-		// Fetch detailed analysis using the provided URL
-		const analysisResponse = await fetch(analysisURL, {
-			headers: {
-				'x-apikey': apiKey.apiKey
-			}
-		});
+		// Check if analysis is completed, otherwise, keep polling until completed
+		while (!analysisData || analysisData.data.attributes.status !== 'completed') {
+			const analysisResponse = await fetch(response.data.links.self, {
+				headers: {
+					'x-apikey': apiKey.apiKey
+				}
+			});
+			analysisData = await analysisResponse.json();
+			console.log(analysisData.data.attributes.status);
+			// Add a delay before next poll (e.g., wait for 1 second)
+			await new Promise(resolve => setTimeout(resolve, 1000));
+		}
 
-		const analysisData = await analysisResponse.json();
-		//console.log(analysisData);
-		//console.log(analysisData.data.attributes.results);
 		return isMalicious(analysisData);
 	} catch (err) {
 		console.error(err);
